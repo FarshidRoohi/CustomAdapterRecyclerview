70ac6c23abf27e0b91ef1509e1bb189114cf34e0
farshidroohi <farshid.roohi.a@gmail.com>
2019-01-01 14:48:58 +0330

    init library

---
+++ customAdapterRecycleView/src/main/java/ir/farshid_roohi/customadapterrecycleview/AdapterRecyclerView.java
@@ -0,0 +1,10 @@
+package ir.farshid_roohi.customadapterrecycleview;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Farshid Roohi.
+ * CustomAdapterRecyclerView | Copyrights 1/1/19.
+ */
+public abstract class AdapterRecyclerView<T> extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+}
