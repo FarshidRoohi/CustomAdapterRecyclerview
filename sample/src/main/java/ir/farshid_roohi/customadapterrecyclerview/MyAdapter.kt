bc922c11a53ab29f7796c88c0b45e4cb6ccab2f1
Farshidroohi <farshid.roohi.a@gmail.com>
2020-05-18 15:18:07 +0430

    Release Version 2.0.0 Update Readme

--- sample/src/main/java/ir/farshid_roohi/customadapterrecyclerview/MyAdapter.kt
+++ sample/src/main/java/ir/farshid_roohi/customadapterrecyclerview/MyAdapter.kt
@@ -8,7 +8,7 @@ import kotlinx.android.synthetic.main.my_item.view.*
  * Created by Farshid Roohi.
  * CustomAdapterRecyclerView | Copyrights 1/2/19.
  */
-class MyAdapter : AdapterRecyclerView<String?>(R.layout.my_item, R.layout.progress_view, R.layout.item_error, R.id.retryTextView) {
+class MyAdapter : AdapterRecyclerView<String?>(R.layout.my_item, R.layout.progress_view, R.layout.item_error,R.id.btnTrayAgain) {
 
     override fun onBindView(viewHolder: ItemViewHolder, position: Int, context: Context, element: String?) {
         val itemView = viewHolder.itemView
