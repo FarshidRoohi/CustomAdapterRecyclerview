3e77a1d93c5616210f7d6050023b92a14acf65fe
Farshidroohi <farshid.roohi.a@gmail.com>
2020-12-06 18:50:44 +0330

    Cleanups Sample

--- sample/src/main/java/ir/farshid_roohi/customadapterrecyclerview/MainActivity.kt
+++ sample/src/main/java/ir/farshid_roohi/customadapterrecyclerview/MainActivity.kt
@@ -11,7 +11,56 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var myAdapter: MyAdapter
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myAdapter = MyAdapter()
+
+        // onClick Error Button
+        myAdapter.onRetryClicked = {
+            myAdapter.loadingState()
+            // fake request or load other items...
+            recyclerView.postDelayed({
+                myAdapter.loadedState(tempItems)
+            }, 2000)
+        }
+
+        // set Data in List
+        myAdapter.loadedState(tempItems)
+
+        recyclerView.layoutManager = GridLayoutManager(this@MainActivity, 2)
+        recyclerView.adapter = myAdapter
+
+
+        // set onLoad More Listener for Pagination
+        recyclerView.onLoadMoreListener {
+            if (myAdapter.mustLoad) {
+                myAdapter.loadingState()
+                // failed state
+                recyclerView.postDelayed({
+                    myAdapter.failedState()
+                }, 2000)
+            }
+        }
+
+
+        // Set onClick Item Listener
+        recyclerView.onItemClickListener({ position ->
+            if (position == myAdapter.itemCount - 1 && !myAdapter.mustLoad) {
+                return@onItemClickListener
+            }
+            Toast.makeText(applicationContext, "item click :  ${myAdapter.getItem(position)}$position", Toast.LENGTH_SHORT).show()
+        }, { position ->
+            if (position == myAdapter.itemCount - 1 && !myAdapter.mustLoad) {
+                return@onItemClickListener
+            }
+            Toast.makeText(applicationContext, "item long click :  ${myAdapter.getItem(position)}$position", Toast.LENGTH_SHORT).show()
+        })
+    }
+
+
+    // Generate Fake Data For List
     private val tempItems: List<String>
         get() {
             val items: MutableList<String> = ArrayList()
@@ -20,57 +69,4 @@ class MainActivity : AppCompatActivity() {
             }
             return items
         }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        myAdapter = MyAdapter().also { adapter ->
-            adapter.onRetryClicked = {
-                adapter.loadingState()
-                // fake request or load other items...
-                recyclerView.postDelayed({
-                    adapter.loadedState(tempItems)
-                }, 2000)
-            }
-        }
-        myAdapter.loadedState(tempItems)
-
-        initRecyclerView()
-
-    }
-
-    private fun initRecyclerView() {
-
-        recyclerView.apply {
-            layoutManager = GridLayoutManager(this@MainActivity, 2)
-            adapter = myAdapter
-
-            // handled endless recyclerView
-            onLoadMoreListener {
-                if (myAdapter.mustLoad) {
-                    myAdapter.loadingState()
-                    // failed state
-                    recyclerView.postDelayed({
-                        myAdapter.failedState()
-                    }, 2000)
-                }
-            }
-
-            // handled click item in recyclerView
-            onItemClickListener({ position ->
-                if (position == myAdapter.itemCount - 1 && !myAdapter.mustLoad) {
-                    return@onItemClickListener
-                }
-                Toast.makeText(applicationContext, "item click :  ${myAdapter.getItem(position)}$position", Toast.LENGTH_SHORT).show()
-            }, { position ->
-                if (position == myAdapter.itemCount - 1 && !myAdapter.mustLoad) {
-                    return@onItemClickListener
-                }
-                Toast.makeText(applicationContext, "item long click :  ${myAdapter.getItem(position)}$position", Toast.LENGTH_SHORT).show()
-            })
-
-        }
-    }
-
 }
